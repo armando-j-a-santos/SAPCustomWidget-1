e36290ebcab9032df0a54c411071cec0cb9dc5ab
Armando Santos <47860995+armando-j-a-santos@users.noreply.github.com>
2022-08-02 17:40:41 +0100

    Update prodigy.js

--- prodigy.js
+++ prodigy.js
@@ -1,27 +1,60 @@
-(function() {
-let template = document.createElement("template");
-template.innerHTML = `
-<style>
-:host {
-border-radius: 55px;
-border-width: 40px;
-border-color: red;
-border-style: solid;
-display: block;
-}
-</style>
-`;
-class ColoredBox123 extends HTMLElement {
-constructor() {
-super();
-let shadowRoot = this.attachShadow({mode: "open"});
-shadowRoot.appendChild(template.content.cloneNode(true));
-this.addEventListener("click", event => {
-var event = new Event("onClick");
-this.dispatchEvent(event);
-});
-this._props = {};
-}
-}
-customElements.define("com-sap-sample-coloredbox", ColoredBox123);
+(function()  {
+    let tmpl = document.createElement('template');
+    tmpl.innerHTML = `
+      <style>
+      </style>
+    `;
+
+    customElements.define('com-sap-sample-armando', class WidgetTemplate extends HTMLElement {
+
+
+		constructor() {
+			super(); 
+			let shadowRoot = this.attachShadow({mode: "open"});
+			shadowRoot.appendChild(tmpl.content.cloneNode(true));
+		}
+
+
+        //Fired when the widget is added to the html DOM of the page
+        connectedCallback(){
+            this._firstConnection = true;
+            this.redraw();
+        }
+
+         //Fired when the widget is removed from the html DOM of the page (e.g. by hide)
+        disconnectedCallback(){
+        
+        }
+
+         //When the custom widget is updated, the Custom Widget SDK framework executes this function first
+		onCustomWidgetBeforeUpdate(oChangedProperties) {
+
+		}
+
+        //When the custom widget is updated, the Custom Widget SDK framework executes this function after the update
+		onCustomWidgetAfterUpdate(oChangedProperties) {
+            if (this._firstConnection){
+                this.redraw();
+            }
+        }
+        
+        //When the custom widget is removed from the canvas or the analytic application is closed
+        onCustomWidgetDestroy(){
+        }
+
+        //When the custom widget is resized on the canvas, the Custom Widget SDK framework executes the following JavaScript function call on the custom widget
+        // Commented out by default.  If it is enabled, SAP Analytics Cloud will track DOM size changes and call this callback as needed
+        //  If you don't need to react to resizes, you can save CPU by leaving it uncommented.
+        /*
+        onCustomWidgetResize(width, height){
+        
+        }
+        */
+
+        redraw(){
+        }
+    
+    
+    });
+        
 })();
