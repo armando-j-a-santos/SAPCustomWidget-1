cd7ae306101176b9902ba1946615bc4684e3fb72
Armando Santos <47860995+armando-j-a-santos@users.noreply.github.com>
2022-08-02 15:45:51 +0100

    Update boxProdigy.js

--- box/boxProdigy.js
+++ box/boxProdigy.js
@@ -1,47 +1,22 @@
-<!DOCTYPE HTML>
-<html>
-  
-<body style="text-align:center;">
-    <h1 style="color:green;">
-        GeeksforGeeks
-    </h1>
-  
-    <p>
-        HTML | customElements define() method
-    </p>
-  
-    <button onclick="Geeks();">
-        click here
-    </button>
-  
-    <p id="arr"></p>
-  
-    <script>
-        var arr =
-            document.getElementById("arr");
-  
-        // Function to define the element
-        function Geeks() {
-            class CustomEl extends HTMLElement {
-                constructor() {
-                    super();
-                    this.attachShadow({ mode: 'open' });
-                    this.shadowRoot.innerHTML = `
-            <h1 style="color:green;">  
-            GeeksforGeeks Custom
-            Element Data
-                </h1>
-            `;
-                }
-            }
-  
-            // Use the define() method to define
-            // a new element
-            window.customElements.define(
-                'gfg-custom-element', CustomEl);
-        } 
-    </script>
-    <gfg-custom-element></gfg-custom-element>
-</body>
-  
-</html>
+const future = ["hello-world"];
+
+let customElementsRegistry = window.customElements;
+const registry = {};
+registry.define = function(name, constructor, options) {
+  console.log('hijacked')
+  if (future.includes(name)) {
+    customElementsRegistry.define(name, constructor, options);
+  }
+}
+Object.defineProperty(window, "customElements", {
+  get() {
+    return registry
+  }
+})
+
+window.customElements.define("hello-world", class HelloWorld extends HTMLElement {
+  constructor() {
+    super()
+    this.innerHTML = "Hello World"
+  }
+});
