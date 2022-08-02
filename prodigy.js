fbcb276d1216f853454bb4c23d1eaa247962f570
Armando Santos <47860995+armando-j-a-santos@users.noreply.github.com>
2022-08-02 20:58:29 +0100

    Update prodigy.js

--- prodigy.js
+++ prodigy.js
@@ -1,7 +1,7 @@
 (function()  {
     let tmpl = document.createElement('template');
     tmpl.innerHTML = `
-        <h1>Hello World</h1>
+        <h1>Hello World - Armando Jorge A. Santos</h1>
     `;
 
     customElements.define('com-sap-sample-helloworldArm', class HelloWorldArm extends HTMLElement {
