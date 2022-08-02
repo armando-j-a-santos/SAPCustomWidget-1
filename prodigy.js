454a00c03b895d28dc6d660dc3f99c13dc9a347b
Armando Santos <47860995+armando-j-a-santos@users.noreply.github.com>
2022-08-02 17:25:15 +0100

    Update prodigy.js

--- prodigy.js
+++ prodigy.js
@@ -22,17 +22,6 @@ this.dispatchEvent(event);
 });
 this._props = {};
 }
-onCustomWidgetBeforeUpdate(changedProperties) {
-this._props = { ...this._props, ...changedProperties };
-}
-onCustomWidgetAfterUpdate(changedProperties) {
-if ("color" in changedProperties) {
-this.style["background-color"] = changedProperties["color"];
-}
-if ("opacity" in changedProperties) {
-this.style["opacity"] = changedProperties["opacity"];
-}
-}
 }
 customElements.define("com-sap-sample-coloredbox", ColoredBox123);
 })();
